4d42d94f102d6b0a8a4c1e07422ca4daeffebed9
Kai Wang <wangk7@uci.edu>
2017-03-15 22:30:10 -0700

    scatter with vertex data updated

--- src/core/engine.hpp
+++ src/core/engine.hpp
@@ -80,6 +80,43 @@ namespace RStream {
 			atomic_partition_number = num_partitions - 1;
 		}
 
+		/* scatter with vertex data (for graph computation use)*/
+		void scatter(std::function<T*(Edge&, char*)> generate_one_update) {
+			// a pair of <vertex, edge_stream> for each partition
+			concurrent_queue<std::pair<int, int>> * task_queue = new concurrent_queue<std::pair<int, int>>(num_partitions);
+
+			// push task into concurrent queue
+			for(int partition_id = 0; partition_id < num_partitions; partition_id++) {
+				int fd_vertex = open((filename + "." + std::to_string(partition_id) + ".vertex").c_str(), O_RDONLY);
+				int fd_edge = open((filename + "." + std::to_string(partition_id)).c_str(), O_RDONLY);
+				assert(fd_vertex > 0 && fd_edge > 0);
+				task_queue->push(std::make_pair(fd_vertex, fd_edge));
+			}
+
+			// allocate global buffers for shuffling
+			global_buffer<T> ** buffers_for_shuffle = buffer_manager<T>::get_global_buffers(num_partitions);
+
+			// exec threads will produce updates and push into shuffle buffers
+			std::vector<std::thread> exec_threads;
+			for(int i = 0; i < num_exec_threads; i++)
+				exec_threads.push_back( std::thread([=] { this->scatter_producer(generate_one_update, buffers_for_shuffle, task_queue); } ));
+
+			// write threads will flush shuffle buffer to update out stream file as long as it's full
+			std::vector<std::thread> write_threads;
+			for(int i = 0; i < num_write_threads; i++)
+				write_threads.push_back(std::thread(&engine::scatter_consumer, this, buffers_for_shuffle));
+
+			// join all threads
+			for(auto & t : exec_threads)
+				t.join();
+
+			for(auto &t : write_threads)
+				t.join();
+
+			delete[] buffers_for_shuffle;
+		}
+
+		/* scatter without vertex data (for relational algebra use)*/
 		void scatter(std::function<T*(Edge&)> generate_one_update) {
 			concurrent_queue<int> * task_queue = new concurrent_queue<int>(num_partitions);
 
@@ -101,7 +138,8 @@ namespace RStream {
 			// exec threads will produce updates and push into shuffle buffers
 			std::vector<std::thread> exec_threads;
 			for(int i = 0; i < num_exec_threads; i++)
-				exec_threads.push_back(std::thread(&engine::scatter_producer, this, generate_one_update, buffers_for_shuffle, task_queue));
+				exec_threads.push_back(std::thread([=] { this->scatter_producer(generate_one_update, buffers_for_shuffle, task_queue); }));
+
 
 			// write threads will flush shuffle buffer to update out stream file as long as it's full
 			std::vector<std::thread> write_threads;
@@ -146,6 +184,55 @@ namespace RStream {
 
 	protected:
 
+		/* scatter producer with vertex data*/
+		//each exec thread generates a scatter_producer
+		void scatter_producer(std::function<T*(Edge&, char*)> generate_one_update,
+				global_buffer<T> ** buffers_for_shuffle, concurrent_queue<std::pair<int, int>> * task_queue) {
+			atomic_num_producers++;
+			std::pair<int, int> fd_pair(-1, -1);
+			// pop from queue
+			while(task_queue->test_pop_atomic(fd_pair)){
+				int fd_vertex = fd_pair.first;
+				int fd_edge = fd_pair.second;
+				assert(fd_vertex > 0 && fd_edge > 0 );
+
+				// get file size
+				size_t vertex_file_size = io_manager::get_filesize(fd_vertex);
+				size_t edge_file_size = io_manager::get_filesize(fd_edge);
+
+				// read from files to thread local buffer
+				char * vertex_local_buf = new char[vertex_file_size];
+				io_manager::read_from_file(fd_vertex, vertex_local_buf, vertex_file_size);
+				char * edge_local_buf = new char[edge_file_size];
+				io_manager::read_from_file(fd_edge, edge_local_buf, edge_file_size);
+
+				// for each edge
+				for(size_t pos = 0; pos < edge_file_size; pos += edge_unit) {
+					// get an edge
+					Edge e = *(Edge*)(edge_local_buf + pos);
+//					std::cout << e << std::endl;
+
+					// gen one update
+					T * update_info = generate_one_update(e, vertex_local_buf);
+//					std::cout << update_info->target << std::endl;
+
+
+					// insert into shuffle buffer accordingly
+					int index = get_global_buffer_index(update_info);
+					global_buffer<T>* global_buf = buffer_manager<T>::get_global_buffer(buffers_for_shuffle, num_partitions, index);
+					global_buf->insert(update_info, index);
+				}
+
+//				std::cout << std::endl;
+				close(fd_vertex);
+				close(fd_edge);
+
+			}
+			atomic_num_producers--;
+
+		}
+
+		/* scatter producer without vertex data*/
 		// each exec thread generates a scatter_producer
 		void scatter_producer(std::function<T*(Edge&)> generate_one_update,
 				global_buffer<T> ** buffers_for_shuffle, concurrent_queue<int> * task_queue) {
@@ -254,6 +341,9 @@ namespace RStream {
 				// delete
 				delete[] vertex_local_buf;
 				delete[] update_local_buf;
+
+				close(fd_vertex);
+				close(fd_update);
 			}
 		}
 
--- src/examples/pagerank.cpp
+++ src/examples/pagerank.cpp
@@ -36,7 +36,8 @@ void apply_one_update(T & update, char * vertex) {
 int main(int argc, const char ** argv) {
 	engine graph_engine("/home/icuzzq/Workspace/git/RStream/input/input");
 	std::function<T*(Edge&)> gen_update = generate_one_update;
-	graph_engine.scatter(generate_one_update);
+//	graph_engine.scatter(generate_one_update);
+
 }
 
 
