68353ff27cd2512c6392826bc3d8df2e38e8c4dc
Kai Wang <wangk7@uci.edu>
2017-03-15 21:20:42 -0700

    gather updated

--- src/core/engine.hpp
+++ src/core/engine.hpp
@@ -89,6 +89,7 @@ namespace RStream {
 			// push task into concurrent queue
 			for(int partition_id = 0; partition_id < num_partitions; partition_id++) {
 				int fd = open((filename + "." + std::to_string(partition_id)).c_str(), O_RDONLY);
+				assert(fd > 0);
 				task_queue->push(fd);
 			}
 
@@ -117,26 +118,27 @@ namespace RStream {
 			delete[] buffers_for_shuffle;
 		}
 
-//		void gather(std::function<void(Edge&)> apply_one_update) {
-//			// a pair of <vertex, update_stream> for each partition
-//			concurrent_queue<std::pair<int, int>> * task_queue = new concurrent_queue<std::pair<int, int>>(num_partitions);
-//
-//			// push task into concurrent queue
-//			for(int partition_id = 0; partition_id < num_partitions; partition_id++) {
-//				int fd_vertex = open((filename + "." + std::to_string(partition_id) + ".vertex").c_str(), O_RDONLY);
-//				int fd_update = open((filename + "." + std::to_string(partition_id) + ".update_stream").c_str(), O_RDONLY);
-//				task_queue->push(std::make_pair(fd_vertex, fd_update));
-//			}
-//
-//			// threads will load vertex and update, and apply update one by one
-//			std::vector<std::thread> threads;
-//			for(int i = 0; i < num_threads; i++)
-//				threads.push_back(std::thread(&engine::gather_producer, this, apply_one_update, task_queue));
-//
-//			// join all threads
-//			for(auto & t : threads)
-//				t.join();
-//		}
+		void gather(std::function<void(T&, char*)> apply_one_update) {
+			// a pair of <vertex, update_stream> for each partition
+			concurrent_queue<std::pair<int, int>> * task_queue = new concurrent_queue<std::pair<int, int>>(num_partitions);
+
+			// push task into concurrent queue
+			for(int partition_id = 0; partition_id < num_partitions; partition_id++) {
+				int fd_vertex = open((filename + "." + std::to_string(partition_id) + ".vertex").c_str(), O_RDONLY);
+				int fd_update = open((filename + "." + std::to_string(partition_id) + ".update_stream").c_str(), O_RDONLY);
+				assert(fd_vertex > 0 && fd_update > 0);
+				task_queue->push(std::make_pair(fd_vertex, fd_update));
+			}
+
+			// threads will load vertex and update, and apply update one by one
+			std::vector<std::thread> threads;
+			for(int i = 0; i < num_threads; i++)
+				threads.push_back(std::thread(&engine::gather_producer, this, apply_one_update, task_queue));
+
+			// join all threads
+			for(auto & t : threads)
+				t.join();
+		}
 
 		void join() {
 
@@ -219,38 +221,41 @@ namespace RStream {
 			}
 		}
 
-//		void gather_producer(std::function<void(T&)> apply_one_update,
-//				concurrent_queue<std::pair<int, int>> * task_queue) {
-//
-//			// pop from queue
-//			std::pair<int, int> fd_pair = task_queue->pop();
-//			int fd_vertex = fd_pair.first;
-//			int fd_update = fd_pair.second;
-//
-//			// get file size
-//			size_t vertex_file_size = io_manager::get_filesize(fd_vertex);
-//			size_t update_file_size = io_manager::get_filesize(fd_update);
-//
-//			// read from files to thread local buffer
-//			char * vertex_local_buf = new char[vertex_file_size];
-//			io_manager::read_from_file(fd_vertex, vertex_local_buf, vertex_file_size);
-//			char * update_local_buf = new char[update_file_size];
-//			io_manager::read_from_file(fd_update, update_local_buf, update_file_size);
-//
-//			// for each update
-//			for(long pos = 0; pos <= update_file_size; pos += sizeof(T)) {
-//				// get an update
-//				T & update = *(T*)(update_local_buf + pos);
-////				apply_one_update(update, vertex_local_buf);
-//			}
-//
-//			// write updated vertex value to disk
-//			io_manager::write_to_file(fd_vertex, vertex_local_buf, vertex_file_size);
-//
-//			// delete
-//			delete[] vertex_local_buf;
-//			delete[] update_local_buf;
-//		}
+		void gather_producer(std::function<void(T&, char*)> apply_one_update,
+				concurrent_queue<std::pair<int, int>> * task_queue) {
+
+			std::pair<int, int> fd_pair(-1, -1);
+			while(task_queue->test_pop_atomic(fd_pair)) {
+				int fd_vertex = fd_pair.first;
+				int fd_update = fd_pair.second;
+				assert(fd_vertex > 0 && fd_update > 0 );
+
+				// get file size
+				size_t vertex_file_size = io_manager::get_filesize(fd_vertex);
+				size_t update_file_size = io_manager::get_filesize(fd_update);
+
+				// read from files to thread local buffer
+				char * vertex_local_buf = new char[vertex_file_size];
+				io_manager::read_from_file(fd_vertex, vertex_local_buf, vertex_file_size);
+				char * update_local_buf = new char[update_file_size];
+				io_manager::read_from_file(fd_update, update_local_buf, update_file_size);
+
+				// for each update
+				// size_t is unsigend int, too small for file size?
+				for(size_t pos = 0; pos < update_file_size; pos += sizeof(T)) {
+					// get an update
+					T & update = *(T*)(update_local_buf + pos);
+					apply_one_update(update, vertex_local_buf);
+				}
+
+				// write updated vertex value to disk
+				io_manager::write_to_file(fd_vertex, vertex_local_buf, vertex_file_size);
+
+				// delete
+				delete[] vertex_local_buf;
+				delete[] update_local_buf;
+			}
+		}
 
 		void join_producer() {
 
--- src/examples/pagerank.cpp
+++ src/examples/pagerank.cpp
@@ -29,6 +29,10 @@ Update* generate_one_update(Edge & e)
 	return update;
 }
 
+void apply_one_update(T & update, char * vertex) {
+
+}
+
 int main(int argc, const char ** argv) {
 	engine graph_engine("/home/icuzzq/Workspace/git/RStream/input/input");
 	std::function<T*(Edge&)> gen_update = generate_one_update;
