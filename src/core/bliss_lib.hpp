77a3d042fe9b9c9727f40a2f6a7ea5b29c091b10
Zhiqiang Zuo <sunzzq@gmail.com>
2017-07-03 18:24:35 -0700

    update bliss support (unfinished)

--- src/core/bliss_lib.hpp
+++ src/core/bliss_lib.hpp
@@ -9,18 +9,107 @@
 #define SRC_CORE_BLISS_LIB_HPP_
 
 #include "../common/RStreamCommon.hpp"
-#include "mining_phase.hpp"
+#include "type.hpp"
 
+#include "defs.hh"
+#include "graph.hh"
+#include "timer.hh"
+#include "utils.hh"
 
-class Bliss_Lib{
+static bool opt_directed = false;
+
+class Bliss_Lib {
+
+	/**
+	 * The hook function that prints the found automorphisms.
+	 * \a param must be a file descriptor (FILE *).
+	 */
+	static void report_aut(void* param, const unsigned int n,
+			const unsigned int* aut) {
+		assert(param);
+		fprintf((FILE*) param, "Generator: ");
+		bliss::print_permutation((FILE*) param, n, aut, 1);
+		fprintf((FILE*) param, "\n");
+	}
 
 public:
-	static std::vector<Element_In_Tuple> & turn_canonical_graph(std::vector<Element_In_Tuple> & sub_graph) {
 
-		return nullptr;
+	static std::vector<Element_In_Tuple> turn_canonical_graph(std::vector<Element_In_Tuple> & sub_graph, std::string & outfile) {
+		bliss::AbstractGraph* aGraph = 0;
+
+		//read graph from tuple
+		aGraph = readGraph(sub_graph, opt_directed);
+		writeOut(aGraph, outfile);
+
+		//canonical labeling
+		bliss::Stats stats;
+		const unsigned int* cl = aGraph->canonical_form(stats, &report_aut, stdout);
+
+		//permute to canonical form
+		bliss::AbstractGraph* cf = aGraph->permute(cl);
+		writeOut(cf, outfile.append(".cano"));
+
+		//write back to tuple
+		std::vector<Element_In_Tuple> out = writeGraph(cf);
+
+		delete cf;
+		delete aGraph;
+
+		return out;
 	}
+
+private:
+
+	static bliss::AbstractGraph* readGraph(std::vector<Element_In_Tuple> & sub_graph, bool opt_directed){
+		bliss::AbstractGraph* g = 0;
+
+		//get the number of vertices
+		std::unordered_map<VertexId, BYTE> vertices;
+		for(unsigned int index = 0; index < sub_graph.size(); ++index){
+			Element_In_Tuple tuple = sub_graph[index];
+			vertices[tuple.vertex_id] = tuple.vertex_label;
+		}
+
+		//construct graph
+		const unsigned int number_vertices = vertices.size();
+		if(opt_directed){
+			g = new bliss::Digraph(vertices.size());
+		}
+		else{
+			g = new bliss::Graph(vertices.size());
+		}
+
+		//set vertices
+		for(unsigned int i = 0; i < number_vertices; ++i){
+			g->change_color(i, vertices[i + 1]);
+		}
+
+		//read edges
+		assert(sub_graph.size() > 1);
+		for(unsigned int index = 1; index < sub_graph.size(); ++index){
+			Element_In_Tuple tuple = sub_graph[index];
+			VertexId from = sub_graph[tuple.history_info].vertex_id;
+			VertexId to = tuple.vertex_id;
+			g->add_edge(from - 1, to - 1);
+		}
+
+		return g;
+	}
+
+	static std::vector<Element_In_Tuple> writeGraph(bliss::AbstractGraph* cf){
+		std::vector<Element_In_Tuple> out;
+		return out;
+	}
+
+
+	static void writeOut(bliss::AbstractGraph* graph, std::string& outfile){
+		FILE* const fp = fopen(outfile.c_str(), "w");
+		if (!fp)
+			printf("Cannot open '%s' for outputting the canonical form, aborting", outfile.c_str());
+		graph->write_dimacs(fp);
+		fclose(fp);
+	}
+
 };
-
-
 
 #endif /* SRC_CORE_BLISS_LIB_HPP_ */
--- src/examples/trianglecouting.cpp
+++ src/examples/trianglecouting.cpp
@@ -1,137 +1,137 @@
-
-#include "../core/engine.hpp"
-#include "../core/scatter.hpp"
-#include "../core/relation_phase.hpp"
-//#include "BaseApplication.hpp"
-
-using namespace RStream;
-
-struct RInUpdate_TriC : BaseUpdate {
-	VertexId src;
-
-	RInUpdate_TriC() : BaseUpdate () {
-//		target = 0;
-//		src = 0;
-	}
-
-	RInUpdate_TriC(VertexId t, VertexId s) : BaseUpdate(t){
-		src = s;
-	}
-};
-
-inline std::ostream & operator<<(std::ostream & strm, const RInUpdate_TriC& update){
-	strm << "(" << update.target << ", " << update.src << ")";
-	return strm;
-}
-
-struct ROutUpdate_TriC : BaseUpdate {
-	VertexId src1;
-	VertexId src2;
-
-	ROutUpdate_TriC() : BaseUpdate () {
-//		target = 0;
-//		src1 = 0;
-//		src2 = 0;
-	}
-
-	ROutUpdate_TriC(VertexId t, VertexId s1, VertexId s2) : BaseUpdate(t){
-		src1 = s1;
-		src2 = s2;
-	}
-
-};
-
-inline std::ostream & operator<<(std::ostream & strm, const ROutUpdate_TriC& update){
-	strm << "(" << update.target << ", " << update.src1 << ", " << update.src2 << ")";
-	return strm;
-}
-
-
-RInUpdate_TriC* generate_one_update(Edge * e)
-{
-	RInUpdate_TriC* update = new RInUpdate_TriC(e->target, e->src);
-	return update;
-}
-
-
-class R1 : public RPhase<RInUpdate_TriC, ROutUpdate_TriC> {
-public:
-	R1(Engine & e) : RPhase(e) {};
-	~R1(){};
-
-	bool filter(RInUpdate_TriC * update, Edge * edge) {
-		if(update->src > update->target || edge->src > edge->target) return true;
-		return false;
-	}
-
-	ROutUpdate_TriC * project_columns(RInUpdate_TriC * in_update, Edge * edge) {
-		ROutUpdate_TriC * new_update = new ROutUpdate_TriC(edge->target, in_update->src, in_update->target);
-		return new_update;
-	}
-};
-
-class R2 : public RPhase<ROutUpdate_TriC, ROutUpdate_TriC> {
-public:
-	R2(Engine & e) : RPhase(e) {};
-	~R2(){};
-
-	bool filter(ROutUpdate_TriC * update, Edge * edge) {
-		if(update->src1 != edge->target) return true;
-		return false;
-	}
-
-	ROutUpdate_TriC * project_columns(ROutUpdate_TriC * in_update, Edge * edge) {
-		return in_update;
-	}
-};
-
-template<typename T>
-void printUpdateStream(int num_partitions, std::string fileName, Update_Stream in_stream){
-	for(int i = 0; i < num_partitions; i++) {
-		std::cout << "--------------------" + (fileName + "." + std::to_string(i) + ".update_stream_" + std::to_string(in_stream)) + "---------------------\n";
-		int fd_update = open((fileName + "." + std::to_string(i) + ".update_stream_" + std::to_string(in_stream)).c_str(), O_RDONLY);
-		assert(fd_update > 0 );
-
-		// get file size
-		long update_file_size = io_manager::get_filesize(fd_update);
-
-		char * update_local_buf = new char[update_file_size];
-		io_manager::read_from_file(fd_update, update_local_buf, update_file_size, 0);
-
-		// for each update
-		for(size_t pos = 0; pos < update_file_size; pos += sizeof(T)) {
-			// get an update
-			T & update = *(T*)(update_local_buf + pos);
-			std::cout << update << std::endl;
-		}
-	}
-}
-
-
-int main(int argc, char ** argv) {
-
-	Engine e("/home/icuzzq/Workspace/git/RStream/input/input_new.txt", 3, 6);
-
-//	Engine e("/home/kai/workspace/dataset/test.txt", 3, 10);
-
-	//scatter phase first to generate updates
-	Scatter<BaseVertex, RInUpdate_TriC> scatter_phase(e);
-	Update_Stream in_stream = scatter_phase.scatter_no_vertex(generate_one_update);
-	printUpdateStream<RInUpdate_TriC>(e.num_partitions, e.filename, in_stream);
-
-	//relational phase 1
-	R1 r1(e);
-	Update_Stream out_stream_1 = r1.join(in_stream);
-	printUpdateStream<ROutUpdate_TriC>(e.num_partitions, e.filename, out_stream_1);
-
-	//relational phase 2
-	R2 r2(e);
-	Update_Stream out_stream_2 = r2.join(out_stream_1);
-	printUpdateStream<ROutUpdate_TriC>(e.num_partitions, e.filename, out_stream_2);
-
-
-}
-
-
-
-
+//
+//#include "../core/engine.hpp"
+//#include "../core/scatter.hpp"
+//#include "../core/relation_phase.hpp"
+////#include "BaseApplication.hpp"
+//
+//using namespace RStream;
+//
+//struct RInUpdate_TriC : BaseUpdate {
+//	VertexId src;
+//
+//	RInUpdate_TriC() : BaseUpdate () {
+////		target = 0;
+////		src = 0;
+//	}
+//
+//	RInUpdate_TriC(VertexId t, VertexId s) : BaseUpdate(t){
+//		src = s;
+//	}
+//};
+//
+//inline std::ostream & operator<<(std::ostream & strm, const RInUpdate_TriC& update){
+//	strm << "(" << update.target << ", " << update.src << ")";
+//	return strm;
+//}
+//
+//struct ROutUpdate_TriC : BaseUpdate {
+//	VertexId src1;
+//	VertexId src2;
+//
+//	ROutUpdate_TriC() : BaseUpdate () {
+////		target = 0;
+////		src1 = 0;
+////		src2 = 0;
+//	}
+//
+//	ROutUpdate_TriC(VertexId t, VertexId s1, VertexId s2) : BaseUpdate(t){
+//		src1 = s1;
+//		src2 = s2;
+//	}
+//
+//};
+//
+//inline std::ostream & operator<<(std::ostream & strm, const ROutUpdate_TriC& update){
+//	strm << "(" << update.target << ", " << update.src1 << ", " << update.src2 << ")";
+//	return strm;
+//}
+//
+//
+//RInUpdate_TriC* generate_one_update(Edge * e)
+//{
+//	RInUpdate_TriC* update = new RInUpdate_TriC(e->target, e->src);
+//	return update;
+//}
+//
+//
+//class R1 : public RPhase<RInUpdate_TriC, ROutUpdate_TriC> {
+//public:
+//	R1(Engine & e) : RPhase(e) {};
+//	~R1(){};
+//
+//	bool filter(RInUpdate_TriC * update, Edge * edge) {
+//		if(update->src > update->target || edge->src > edge->target) return true;
+//		return false;
+//	}
+//
+//	ROutUpdate_TriC * project_columns(RInUpdate_TriC * in_update, Edge * edge) {
+//		ROutUpdate_TriC * new_update = new ROutUpdate_TriC(edge->target, in_update->src, in_update->target);
+//		return new_update;
+//	}
+//};
+//
+//class R2 : public RPhase<ROutUpdate_TriC, ROutUpdate_TriC> {
+//public:
+//	R2(Engine & e) : RPhase(e) {};
+//	~R2(){};
+//
+//	bool filter(ROutUpdate_TriC * update, Edge * edge) {
+//		if(update->src1 != edge->target) return true;
+//		return false;
+//	}
+//
+//	ROutUpdate_TriC * project_columns(ROutUpdate_TriC * in_update, Edge * edge) {
+//		return in_update;
+//	}
+//};
+//
+//template<typename T>
+//void printUpdateStream(int num_partitions, std::string fileName, Update_Stream in_stream){
+//	for(int i = 0; i < num_partitions; i++) {
+//		std::cout << "--------------------" + (fileName + "." + std::to_string(i) + ".update_stream_" + std::to_string(in_stream)) + "---------------------\n";
+//		int fd_update = open((fileName + "." + std::to_string(i) + ".update_stream_" + std::to_string(in_stream)).c_str(), O_RDONLY);
+//		assert(fd_update > 0 );
+//
+//		// get file size
+//		long update_file_size = io_manager::get_filesize(fd_update);
+//
+//		char * update_local_buf = new char[update_file_size];
+//		io_manager::read_from_file(fd_update, update_local_buf, update_file_size, 0);
+//
+//		// for each update
+//		for(size_t pos = 0; pos < update_file_size; pos += sizeof(T)) {
+//			// get an update
+//			T & update = *(T*)(update_local_buf + pos);
+//			std::cout << update << std::endl;
+//		}
+//	}
+//}
+//
+//
+//int main(int argc, char ** argv) {
+//
+//	Engine e("/home/icuzzq/Workspace/git/RStream/input/input_new.txt", 3, 6);
+//
+////	Engine e("/home/kai/workspace/dataset/test.txt", 3, 10);
+//
+//	//scatter phase first to generate updates
+//	Scatter<BaseVertex, RInUpdate_TriC> scatter_phase(e);
+//	Update_Stream in_stream = scatter_phase.scatter_no_vertex(generate_one_update);
+//	printUpdateStream<RInUpdate_TriC>(e.num_partitions, e.filename, in_stream);
+//
+//	//relational phase 1
+//	R1 r1(e);
+//	Update_Stream out_stream_1 = r1.join(in_stream);
+//	printUpdateStream<ROutUpdate_TriC>(e.num_partitions, e.filename, out_stream_1);
+//
+//	//relational phase 2
+//	R2 r2(e);
+//	Update_Stream out_stream_2 = r2.join(out_stream_1);
+//	printUpdateStream<ROutUpdate_TriC>(e.num_partitions, e.filename, out_stream_2);
+//
+//
+//}
+//
+//
+//
+//
