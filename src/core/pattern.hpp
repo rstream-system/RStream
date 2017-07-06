e205f8f5056ea332b59073d913329fd895a4e4c0
Zhiqiang Zuo <sunzzq@gmail.com>
2017-07-05 22:06:38 -0700

    update

--- src/core/pattern.hpp
+++ src/core/pattern.hpp
@@ -49,6 +49,32 @@ public:
 
 
 	static bool is_automorphism(std::vector<Element_In_Tuple> & sub_graph) {
+		assert(sub_graph.size() > 2);
+
+		Element_In_Tuple last_tuple = sub_graph.back();
+		std::pair<VertexId, VertexId> added_edge = getEdge(sub_graph, sub_graph.size() - 1);
+
+		//check to see if there already exists the vertex added; if so, just allow to add edge which is (smaller id -> bigger id)
+		for(unsigned int i = 0; i < sub_graph.size() - 1; ++i){
+			if(sub_graph[i].vertex_id == last_tuple.vertex_id){
+				if(added_edge.first > added_edge.second){
+					return true;
+				}
+				else{
+					break;
+				}
+			}
+		}
+
+		for(unsigned int index = last_tuple.history_info + 1; index < sub_graph.size(); ++index){
+			std::pair<VertexId, VertexId> edge = getEdge(sub_graph, index);
+			int cmp = compare(added_edge, edge);
+			assert(cmp != 0);
+			if(cmp < 0){
+				return true;
+			}
+		}
+
 		return false;
 	}
 
@@ -98,6 +124,32 @@ public:
 
 
 private:
+
+	//generate a pair which consists of <keyId, addedId>
+	static std::pair<VertexId, VertexId> getEdge(std::vector<Element_In_Tuple> & sub_graph, unsigned int index){
+		Element_In_Tuple tuple = sub_graph[index];
+		std::pair<VertexId, VertexId> * edge = new std::pair<VertexId, VertexId>(sub_graph[tuple.history_info].vertex_id, tuple.vertex_id);
+		return *edge;
+	}
+
+	static void swap(std::pair<VertexId, VertexId>& pair){
+		if (pair.first > pair.second) {
+			VertexId tmp = pair.first;
+			pair.first = pair.second;
+			pair.second = tmp;
+		}
+	}
+	static int compare(std::pair<VertexId, VertexId>& oneEdge, std::pair<VertexId, VertexId>& otherEdge){
+		swap(oneEdge);
+		swap(otherEdge);
+
+		if(oneEdge.first == otherEdge.first){
+			return oneEdge.second - otherEdge.second;
+		}
+		else{
+			return oneEdge.first - otherEdge.first;
+		}
+	}
 
 	static bliss::AbstractGraph* readGraph(std::vector<Element_In_Tuple> & sub_graph, bool opt_directed){
 		bliss::AbstractGraph* g = 0;
