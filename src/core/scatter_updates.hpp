a09415f159173ab02b015b7ca03aa61b7d3c05b9
Kai Wang <wangk7@uci.edu>
2017-07-11 16:20:41 -0700

    add transitive closure

---
+++ src/core/scatter_updates.hpp
@@ -0,0 +1,174 @@
+/*
+ * scatter_updates.hpp
+ *
+ *  Created on: Jul 11, 2017
+ *      Author: kai
+ */
+
+#ifndef CORE_SCATTER_UPDATES_HPP_
+#define CORE_SCATTER_UPDATES_HPP_
+
+#include "io_manager.hpp"
+#include "buffer_manager.hpp"
+#include "concurrent_queue.hpp"
+#include "type.hpp"
+#include "constants.hpp"
+
+namespace RStream {
+
+	template <typename InUpdateType, typename OutUpdateType>
+	class Scatter_Updates {
+		const Engine& context;
+
+		std::atomic<int> atomic_num_producers;
+		std::atomic<int> atomic_partition_id;
+		std::atomic<int> atomic_partition_number;
+
+	public:
+
+		Scatter_Updates(Engine & e) : context(e) {
+			atomic_num_producers = 0;
+			atomic_partition_id = 0;
+			atomic_partition_number = context.num_partitions;
+		};
+
+		/*
+		 * given an in_update, generate an out_update and scatter
+		 * */
+		Update_Stream scatter_updates(Update_Stream in_update_stream, std::function<OutUpdateType*(InUpdateType*)> generate_one_update) {
+			Update_Stream update_c = Engine::update_count++;
+
+			concurrent_queue<int> * task_queue = new concurrent_queue<int>(context.num_partitions);
+
+			// allocate global buffers for shuffling
+			global_buffer<OutUpdateType> ** buffers_for_shuffle = buffer_manager<OutUpdateType>::get_global_buffers(context.num_partitions);
+
+			// push task into concurrent queue
+			for(int partition_id = 0; partition_id < context.num_partitions; partition_id++) {
+				task_queue->push(partition_id);
+			}
+
+			// exec threads will produce updates and push into shuffle buffers
+			std::vector<std::thread> exec_threads;
+			for(int i = 0; i < context.num_exec_threads; i++)
+				exec_threads.push_back(std::thread([=] { this->scatter_updates_producer(in_update_stream, generate_one_update, buffers_for_shuffle, task_queue); }));
+
+			// write threads will flush shuffle buffer to update out stream file as long as it's full
+			std::vector<std::thread> write_threads;
+			for(int i = 0; i < context.num_write_threads; i++)
+				write_threads.push_back(std::thread(&Scatter_Updates::scatter_updates_consumer, this, buffers_for_shuffle, update_c));
+
+			// join all threads
+			for(auto & t : exec_threads)
+				t.join();
+
+			for(auto &t : write_threads)
+				t.join();
+
+			delete[] buffers_for_shuffle;
+			delete task_queue;
+
+			return update_c;
+		}
+
+	private:
+		void scatter_updates_producer(Update_Stream in_update_stream, std::function<OutUpdateType*(InUpdateType*)> generate_one_update,
+						global_buffer<OutUpdateType> ** buffers_for_shuffle, concurrent_queue<int> * task_queue) {
+
+			atomic_num_producers++;
+			int partition_id = -1;
+
+			// pop from queue
+			while(task_queue->test_pop_atomic(partition_id)){
+				int fd_update = open((context.filename + "." + std::to_string(partition_id) + ".update_stream_" + std::to_string(in_update_stream)).c_str(), O_RDONLY);
+				assert(fd_update > 0);
+
+				// get file size
+				long update_file_size = io_manager::get_filesize(fd_update);
+
+				// streaming updates
+				char * update_local_buf = (char *)memalign(PAGE_SIZE, IO_SIZE);
+				int streaming_counter = update_file_size / IO_SIZE + 1;
+
+				long valid_io_size = 0;
+				long offset = 0;
+
+				// for all streaming updates
+				for(int counter = 0; counter < streaming_counter; counter++) {
+					// last streaming
+					if(counter == streaming_counter - 1)
+						// TODO: potential overflow?
+						valid_io_size = update_file_size - IO_SIZE * (streaming_counter - 1);
+					else
+						valid_io_size = IO_SIZE;
+
+					assert(valid_io_size % sizeof(InUpdateType) == 0);
+
+					io_manager::read_from_file(fd_update, update_local_buf, valid_io_size, offset);
+					offset += valid_io_size;
+
+					// streaming updates in
+					for(long pos = 0; pos < valid_io_size; pos += sizeof(InUpdateType)) {
+						// get an in_update
+						InUpdateType * in_update = (InUpdateType*)(update_local_buf + pos);
+
+						// gen an out_update
+						OutUpdateType * out_update = generate_one_update(in_update);
+
+						// insert into shuffle buffer accordingly
+						int index = get_global_buffer_index(out_update);
+						global_buffer<OutUpdateType>* global_buf = buffer_manager<OutUpdateType>::get_global_buffer(buffers_for_shuffle, context.num_partitions, index);
+						global_buf->insert(out_update, index);
+
+					}
+				}
+
+				delete[] update_local_buf;
+				close(fd_update);
+			}
+
+			atomic_num_producers--;
+		}
+
+		// each writer thread generates a scatter_consumer
+		void scatter_updates_consumer(global_buffer<OutUpdateType> ** buffers_for_shuffle, Update_Stream update_count) {
+			while(atomic_num_producers != 0) {
+				int i = (atomic_partition_id++) % context.num_partitions ;
+
+				const char * file_name = (context.filename + "." + std::to_string(i) + ".update_stream_" + std::to_string(update_count)).c_str();
+				global_buffer<OutUpdateType>* g_buf = buffer_manager<OutUpdateType>::get_global_buffer(buffers_for_shuffle, context.num_partitions, i);
+				g_buf->flush(file_name, i);
+			}
+
+			//the last run - deal with all remaining content in buffers
+			while(true){
+				int i = --atomic_partition_number;
+
+				if(i >= 0){
+
+					const char * file_name = (context.filename + "." + std::to_string(i) + ".update_stream_" + std::to_string(update_count)).c_str();
+					global_buffer<OutUpdateType>* g_buf = buffer_manager<OutUpdateType>::get_global_buffer(buffers_for_shuffle, context.num_partitions, i);
+					g_buf->flush_end(file_name, i);
+
+					delete g_buf;
+				}
+				else{
+					break;
+				}
+			}
+		}
+
+
+		int get_global_buffer_index(OutUpdateType* out_update) {
+		//			return update_info->target / context.num_vertices_per_part;
+
+			int partition_id = out_update->target / context.num_vertices_per_part;
+			return partition_id < (context.num_partitions - 1) ? partition_id : (context.num_partitions - 1);
+		}
+
+	};
+}
+
+
+
+#endif /* CORE_SCATTER_UPDATES_HPP_ */
--- src/examples/transitiveclosure.cpp
+++ src/examples/transitiveclosure.cpp
@@ -5,37 +5,108 @@
 // *      Author: kai
 // */
 
-//#include "../core/engine.hpp"
-//#include "../core/scatter.hpp"
-//#include "../core/relation_phase.hpp"
-//#include "BaseApplication.hpp"
-//
-//using namespace RStream;
-//
-//struct Update_Stream_TC {
+#include "../core/engine.hpp"
+#include "../core/scatter.hpp"
+#include "../core/scatter_updates.hpp"
+#include "../core/relation_phase.hpp"
+
+using namespace RStream;
+
+struct Update_Stream_TC {
+	VertexId src;
+	VertexId target;
+
+	Update_Stream_TC(VertexId _src, VertexId _target) : src(_src), target(_target) {}
+
+	bool operator == (const Update_Stream_TC & obj) const {
+		if(src == obj.src && target == obj.target)
+			return true;
+		else
+			return false;
+	}
+};
+
+typedef Update_Stream_TC In_Update_TC;
+typedef Update_Stream_TC Out_Update_TC;
+
+namespace std {
+	template<>
+	struct hash<Update_Stream_TC> {
+		size_t operator() (const Update_Stream_TC & obj) const {
+			size_t hash = 17;
+			hash = 31 * hash + obj.src;
+			hash = 31 * hash + obj.target;
+
+			return hash;
+		}
+	};
+}
+
+//struct In_Update_TC {
 //	VertexId src;
 //	VertexId target;
-//
-//	bool operator == (const Update_Stream_TC & obj) const {
-//		if(src == obj.src && target == obj.target)
-//			return true;
-//		else
-//			return false;
-//	}
 //};
-//
-//namespace std {
-//	template<>
-//	struct hash<Update_Stream_TC> {
-//		size_t operator() (const Update_Stream_TC & obj) const {
-//			size_t hash = 17;
-//			hash = 31 * hash + obj.src;
-//			hash = 31 * hash + obj.target;
-//
-//			return hash;
-//		}
-//	};
-//}
+
+Out_Update_TC * generate_one_update(Edge * e) {
+	Out_Update_TC * out_update = new Out_Update_TC(e->src, e->target);
+	return new out_update;
+}
+
+Out_Update_TC * generate_out_update(In_Update_TC * in_update) {
+	Out_Update_TC * out_update = new Out_Update_TC(in_update->src, in_update->target);
+	return out_update;
+}
+
+class TC : public RPhase<In_Update_TC, Out_Update_TC> {
+	public:
+		TC(Engine & e) : RPhase(e) {};
+		~TC(){};
+
+		bool filter(In_Update_TC * update, Edge * edge) {
+			return false;
+		}
+
+		Out_Update_TC * project_columns(In_Update_TC * in_update, Edge * edge) {
+			Out_Update_TC * new_update = new Out_Update_TC(in_update->src, edge->target);
+			return new_update;
+		}
+};
+
+bool should_terminate(Update_Stream delta_tc, Engine & e) {
+	for(int i = 0; i < e.num_partitions; i++) {
+		int fd_update = open((e.filename + "." + std::to_string(i) + ".update_stream_" + std::to_string(delta_tc)).c_str(), O_RDONLY);
+
+		if(io_manager::get_filesize(fd_update) > 0) {
+			close(fd_update);
+			return false;
+		}
+
+		close(fd_update);
+	}
+
+	return true;
+}
+
+int main(int argc, char ** argv) {
+	Engine e("/home/icuzzq/Workspace/git/RStream/input/input_new.txt", 3, 6);
+
+	//scatter phase first to generate updates
+	Scatter<BaseVertex, In_Update_TC> scatter_edges(e);
+	Update_Stream delta_tc = scatter_edges.scatter_no_vertex(generate_one_update);
+	Update_Stream tc = scatter_edges.scatter_no_vertex(generate_one_update);
+
+	Scatter_Updates<In_Update_TC, Out_Update_TC> sc_up(e);
+	TC triangle_counting(e);
+
+	while(!should_terminate(delta_tc, e)) {
+
+		Update_Stream out = triangle_counting.join(delta_tc);
+		Update_Stream delta = triangle_counting.set_difference(out, tc);
+		triangle_counting.union_relation(tc, delta);
+		Update_Stream new_delta = sc_up.scatter_updates(delta, generate_out_update);
+		delta_tc = new_delta;
+	}
+}
 
 
 
