450b8ba75b2c56e3b542be3d991ef9a7ea5208cc
Kai Wang <wangk7@uci.edu>
2017-08-04 21:45:01 -0700

    update

--- src/apps/pagerank.cpp
+++ src/apps/pagerank.cpp
@@ -1,75 +1,74 @@
-//	/*
-//	 * pagerank.cpp
-//	 *
-//	 *  Created on: Mar 7, 2017
-//	 *      Author: kai
-//	 */
-//
-//	#include "../core/engine.hpp"
-//	#include "../core/scatter.hpp"
-//	#include "../core/gather.hpp"
-//	//#include "BaseApplication.hpp"
-//
-//	using namespace RStream;
-//
-//	struct Update : BaseUpdate{
-//		float rank;
-//
-//		Update(int _target, float _rank) : BaseUpdate(_target), rank(_rank) {};
-//
-//		Update() : BaseUpdate(0), rank(0.0) {}
-//
-//		std::string toString(){
-//			return "(" + std::to_string(target) + ", " + std::to_string(rank) + ")";
-//		}
-//	}__attribute__((__packed__));
-//
-//
-//	struct Vertex : BaseVertex {
-//		int degree;
-//		float rank;
-//		float sum;
-//	}__attribute__((__packed__));
-//
-//	//inline std::ostream & operator<<(std::ostream & strm, const Vertex& vertex){
-//	//	strm << "[" << vertex.id << "," << vertex.degree << "]";
-//	//	return strm;
-//	//}
-//
-//	void initttt(char* data, VertexId id) {
-//		struct Vertex * v = (struct Vertex*)data;
-//		v->degree = 0;
-//		v->sum = 0;
-//		v->rank = 1.0f;
-//		v->id = id;
-//	}
-//
-//	Update * generate_one_update(Edge * e, Vertex * v) {
-//		Update* update = new Update(e->src, v->rank / v->degree);
-//		return update;
-//	}
-//
-//	void apply_one_update(Update * update, Vertex * dst_vertex) {
-//		dst_vertex->sum += update->rank;
-//		dst_vertex->rank = 0.15 + 0.85 * dst_vertex->sum;
-//	}
-//
-//
-//	int main(int argc, char ** argv) {
-////		Engine e("/home/icuzzq/Workspace/git/RStream/input/input_new.txt", 3, 6);
-//		Engine e(std::string(argv[1]), atoi(argv[2]), atoi(argv[3]));
-////		e.init_vertex<Vertex>(initttt);
-////		e.init_vertex<Vertex>(init);
-////		e.compute_degree<Vertex>();
-//
-//		int num_iters = 5;
-//		for(int i = 0; i < num_iters; i++) {
-//			Scatter<Vertex, Update> scatter_phase(e);
-//			Update_Stream in_stream = scatter_phase.scatter_with_vertex(generate_one_update);
-//			Gather<Vertex, Update> gather_phase(e);
-//			gather_phase.gather(in_stream, apply_one_update);
-//		}
-//	}
-//
-//
-//
+	/*
+	 * pagerank.cpp
+	 *
+	 *  Created on: Mar 7, 2017
+	 *      Author: kai
+	 */
+
+	#include "../core/engine.hpp"
+	#include "../core/scatter.hpp"
+	#include "../core/gather.hpp"
+	//#include "BaseApplication.hpp"
+
+	using namespace RStream;
+
+	struct Update_PR : BaseUpdate{
+		float rank;
+
+		Update_PR(int _target, float _rank) : BaseUpdate(_target), rank(_rank) {};
+
+		Update_PR() : BaseUpdate(0), rank(0.0) {}
+
+		std::string toString(){
+			return "(" + std::to_string(target) + ", " + std::to_string(rank) + ")";
+		}
+	}__attribute__((__packed__));
+
+
+	struct Vertex_PR : BaseVertex {
+		int degree;
+		float rank;
+		float sum;
+	}__attribute__((__packed__));
+
+	//inline std::ostream & operator<<(std::ostream & strm, const Vertex& vertex){
+	//	strm << "[" << vertex.id << "," << vertex.degree << "]";
+	//	return strm;
+	//}
+
+	void init(char* data, VertexId id) {
+		struct Vertex_PR * v = (struct Vertex_PR*)data;
+		v->degree = 0;
+		v->sum = 0;
+		v->rank = 1.0f;
+		v->id = id;
+	}
+
+	Update_PR * generate_one_update(Edge * e, Vertex_PR * v) {
+		Update_PR * update = new Update_PR(e->src, v->rank / v->degree);
+		return update;
+	}
+
+	void apply_one_update(Update_PR * update, Vertex_PR * dst_vertex) {
+		dst_vertex->sum += update->rank;
+		dst_vertex->rank = 0.15 + 0.85 * dst_vertex->sum;
+	}
+
+
+	int main(int argc, char ** argv) {
+//		Engine e("/home/icuzzq/Workspace/git/RStream/input/input_new.txt", 3, 6);
+		Engine e(std::string(argv[1]), atoi(argv[2]), atoi(argv[3]));
+		e.init_vertex<Vertex_PR>(init);
+		e.compute_degree<Vertex_PR>();
+
+		int num_iters = 5;
+		for(int i = 0; i < num_iters; i++) {
+			Scatter<Vertex_PR, Update_PR> scatter_phase(e);
+			Update_Stream in_stream = scatter_phase.scatter_with_vertex(generate_one_update);
+			Gather<Vertex_PR, Update_PR> gather_phase(e);
+			gather_phase.gather(in_stream, apply_one_update);
+		}
+	}
+
+
+
--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -89,185 +89,185 @@ namespace RStream {
 //		}
 
 		/* init vertex data*/
-		template <typename VertexDataType>
-		void Engine::init_vertex(std::function<void(char*, VertexId)> init) {
-			vertex_unit = sizeof(VertexDataType);
-
-			// a pair of <vertex_file, num_vertices>
-//			concurrent_queue<std::pair<int, int>> * task_queue = new concurrent_queue<std::pair<int, int>>(num_partitions);
-
-			// <vertex_file, num_vertices, start_vertex_id>
-			concurrent_queue<std::tuple<int, VertexId, VertexId>> * task_queue = new concurrent_queue<std::tuple<int, VertexId, VertexId>>(num_partitions);
-
-			for(int partition_id = 0; partition_id < num_partitions; partition_id++) {
-				int perms = O_WRONLY;
-				std::string vertex_file = filename + "." + std::to_string(partition_id) + ".vertex";
-				int fd = open(vertex_file.c_str(), perms, S_IRWXU);
-				if(fd < 0) {
-					fd = creat(vertex_file.c_str(), S_IRWXU);
-				}
-
-				VertexId n_vertices = vertex_intervals[partition_id].second - vertex_intervals[partition_id].first + 1;
-//				task_queue->push(std::make_pair(fd, n_vertices));
-				task_queue->push(std::make_tuple(fd, n_vertices, vertex_intervals[partition_id].first));
-
-			}
-
-			// threads will load vertex and update, and apply update one by one
-			std::vector<std::thread> threads;
-			for(int i = 0; i < num_threads; i++)
-				threads.push_back(std::thread(&Engine::init_produer<VertexDataType>, this, init, task_queue));
-
-			// join all threads
-			for(auto & t : threads)
-				t.join();
-		}
+//		template <typename VertexDataType>
+//		void Engine::init_vertex(std::function<void(char*, VertexId)> init) {
+//			vertex_unit = sizeof(VertexDataType);
+//
+//			// a pair of <vertex_file, num_vertices>
+////			concurrent_queue<std::pair<int, int>> * task_queue = new concurrent_queue<std::pair<int, int>>(num_partitions);
+//
+//			// <vertex_file, num_vertices, start_vertex_id>
+//			concurrent_queue<std::tuple<int, VertexId, VertexId>> * task_queue = new concurrent_queue<std::tuple<int, VertexId, VertexId>>(num_partitions);
+//
+//			for(int partition_id = 0; partition_id < num_partitions; partition_id++) {
+//				int perms = O_WRONLY;
+//				std::string vertex_file = filename + "." + std::to_string(partition_id) + ".vertex";
+//				int fd = open(vertex_file.c_str(), perms, S_IRWXU);
+//				if(fd < 0) {
+//					fd = creat(vertex_file.c_str(), S_IRWXU);
+//				}
+//
+//				VertexId n_vertices = vertex_intervals[partition_id].second - vertex_intervals[partition_id].first + 1;
+////				task_queue->push(std::make_pair(fd, n_vertices));
+//				task_queue->push(std::make_tuple(fd, n_vertices, vertex_intervals[partition_id].first));
+//
+//			}
+//
+//			// threads will load vertex and update, and apply update one by one
+//			std::vector<std::thread> threads;
+//			for(int i = 0; i < num_threads; i++)
+//				threads.push_back(std::thread(&Engine::init_produer<VertexDataType>, this, init, task_queue));
+//
+//			// join all threads
+//			for(auto & t : threads)
+//				t.join();
+//		}
 
 		/*compute out degree for each vertex*/
-		template <typename VertexDataType>
-		void Engine::compute_degree() {
+//		template <typename VertexDataType>
+//		void Engine::compute_degree() {
+//
+//			concurrent_queue<int> * task_queue = new concurrent_queue<int>(num_partitions);
+//
+//			// push task into concurrent queue
+//			for(int partition_id = 0; partition_id < num_partitions; partition_id++) {
+//				task_queue->push(partition_id);
+//			}
+//
+//			std::vector<std::thread> threads;
+//			for(int i = 0; i < num_threads; i++)
+//				threads.push_back(std::thread(&Engine::compute_degree_producer<VertexDataType>, this, task_queue));
+//
+//			// join all threads
+//			for(auto & t : threads)
+//				t.join();
+//
+//			delete task_queue;
+//		}
 
-			concurrent_queue<int> * task_queue = new concurrent_queue<int>(num_partitions);
-
-			// push task into concurrent queue
-			for(int partition_id = 0; partition_id < num_partitions; partition_id++) {
-				task_queue->push(partition_id);
-			}
-
-			std::vector<std::thread> threads;
-			for(int i = 0; i < num_threads; i++)
-				threads.push_back(std::thread(&Engine::compute_degree_producer<VertexDataType>, this, task_queue));
-
-			// join all threads
-			for(auto & t : threads)
-				t.join();
-
-			delete task_queue;
-		}
 
 
-
-		template <typename VertexDataType>
-//		void Engine::init_produer(std::function<void(char*, VertexId)> init, concurrent_queue<std::pair<int, int>> * task_queue) {
-		void Engine::init_produer(std::function<void(char*, VertexId)> init, concurrent_queue<std::tuple<int, VertexId, VertexId>> * task_queue) {
-//			std::pair<int, int> pair(-1, -1);
-			int fd = -1;
-			VertexId num_vertex = 0, start_vertex = -1;
-			auto one_task = std::make_tuple(fd, num_vertex, start_vertex);
-
-//			while(task_queue->test_pop_atomic(pair)) {
-			while(task_queue->test_pop_atomic(one_task)) {
-//				int fd = pair.first;
-//				int num_vertex = pair.second;
-//				assert(fd > 0 && num_vertex > 0 );
-
-				fd = std::get<0>(one_task);
-				num_vertex = std::get<1>(one_task);
-				start_vertex = std::get<2>(one_task);
-				assert(fd > 0 && num_vertex > 0 && start_vertex >= 0);
-
-				// size_t ok??
-				size_t vertex_file_size = num_vertex * sizeof(VertexDataType);
-				char * vertex_local_buf = new char[vertex_file_size];
-
-				// for each vertex
+//		template <typename VertexDataType>
+////		void Engine::init_produer(std::function<void(char*, VertexId)> init, concurrent_queue<std::pair<int, int>> * task_queue) {
+//		void Engine::init_produer(std::function<void(char*, VertexId)> init, concurrent_queue<std::tuple<int, VertexId, VertexId>> * task_queue) {
+////			std::pair<int, int> pair(-1, -1);
+//			int fd = -1;
+//			VertexId num_vertex = 0, start_vertex = -1;
+//			auto one_task = std::make_tuple(fd, num_vertex, start_vertex);
+//
+////			while(task_queue->test_pop_atomic(pair)) {
+//			while(task_queue->test_pop_atomic(one_task)) {
+////				int fd = pair.first;
+////				int num_vertex = pair.second;
+////				assert(fd > 0 && num_vertex > 0 );
+//
+//				fd = std::get<0>(one_task);
+//				num_vertex = std::get<1>(one_task);
+//				start_vertex = std::get<2>(one_task);
+//				assert(fd > 0 && num_vertex > 0 && start_vertex >= 0);
+//
+//				// size_t ok??
+//				size_t vertex_file_size = num_vertex * sizeof(VertexDataType);
+//				char * vertex_local_buf = new char[vertex_file_size];
+//
+//				// for each vertex
+////				for(size_t pos = 0; pos < vertex_file_size; pos += sizeof(VertexDataType)) {
+////					init(vertex_local_buf + pos);
+////				}
+//
+//				VertexId counter = 0;
+//				// for each vertex
 //				for(size_t pos = 0; pos < vertex_file_size; pos += sizeof(VertexDataType)) {
-//					init(vertex_local_buf + pos);
+//					init(vertex_local_buf + pos, start_vertex + counter);
+//					counter++;
 //				}
+//
+//				io_manager::write_to_file(fd, vertex_local_buf, vertex_file_size);
+//
+//				delete[] vertex_local_buf;
+//				close(fd);
+//			}
+//		}
 
-				VertexId counter = 0;
-				// for each vertex
-				for(size_t pos = 0; pos < vertex_file_size; pos += sizeof(VertexDataType)) {
-					init(vertex_local_buf + pos, start_vertex + counter);
-					counter++;
-				}
+//		template <typename VertexDataType>
+//		void Engine::load_vertices_hashMap(char* vertex_local_buf, const int vertex_file_size, std::unordered_map<VertexId, VertexDataType*> & vertex_map){
+//			for(size_t off = 0; off < vertex_file_size; off += vertex_unit){
+//				VertexDataType* v = reinterpret_cast<VertexDataType*>(vertex_local_buf + off);
+//				vertex_map[v->id] = v;
+//			}
+//		}
 
-				io_manager::write_to_file(fd, vertex_local_buf, vertex_file_size);
-
-				delete[] vertex_local_buf;
-				close(fd);
-			}
-		}
-
-		template <typename VertexDataType>
-		void Engine::load_vertices_hashMap(char* vertex_local_buf, const int vertex_file_size, std::unordered_map<VertexId, VertexDataType*> & vertex_map){
-			for(size_t off = 0; off < vertex_file_size; off += vertex_unit){
-				VertexDataType* v = reinterpret_cast<VertexDataType*>(vertex_local_buf + off);
-				vertex_map[v->id] = v;
-			}
-		}
-
-		template <typename VertexDataType>
-		void Engine::compute_degree_producer(concurrent_queue<int> * task_queue) {
-			int partition_id = -1;
-			while(task_queue->test_pop_atomic(partition_id)) {
-				int fd_vertex = open((filename + "." + std::to_string(partition_id) + ".vertex").c_str(), O_RDWR);
-				int fd_edge = open((filename + "." + std::to_string(partition_id)).c_str(), O_RDONLY);
-				assert(fd_vertex > 0 && fd_edge > 0 );
-
-				// get file size
-				long vertex_file_size = io_manager::get_filesize(fd_vertex);
-				long edge_file_size = io_manager::get_filesize(fd_edge);
-
-				// vertex data fully loaded into memory
-				char * vertex_local_buf = new char[vertex_file_size];
-				io_manager::read_from_file(fd_vertex, vertex_local_buf, vertex_file_size, 0);
-				std::unordered_map<VertexId, VertexDataType*> vertex_map;
-				load_vertices_hashMap(vertex_local_buf, vertex_file_size, vertex_map);
-
-				// streaming edges
-//				char * edge_local_buf = (char *)memalign(PAGE_SIZE, IO_SIZE);
-//				int streaming_counter = edge_file_size / IO_SIZE + 1;
-				char * edge_local_buf = (char *)memalign(PAGE_SIZE, IO_SIZE * sizeof(Edge));
-				int streaming_counter = edge_file_size / (IO_SIZE * sizeof(Edge)) + 1;
-
-				long valid_io_size = 0;
-				long offset = 0;
-
-				assert(edge_unit == sizeof(Edge));
-				// for all streaming
-				for(int counter = 0; counter < streaming_counter; counter++) {
-
-					// last streaming
-					if(counter == streaming_counter - 1)
-						// TODO: potential overflow?
-//						valid_io_size = edge_file_size - IO_SIZE * (streaming_counter - 1);
-						valid_io_size = edge_file_size - IO_SIZE * sizeof(Edge) * (streaming_counter - 1);
-					else
-//						valid_io_size = IO_SIZE;
-						valid_io_size = IO_SIZE * sizeof(Edge);
-
-					assert(valid_io_size % sizeof(edge_unit) == 0);
-
-					io_manager::read_from_file(fd_edge, edge_local_buf, valid_io_size, offset);
-					offset += valid_io_size;
-
-					for(long pos = 0; pos < valid_io_size; pos += edge_unit) {
-						// get an edge
-						Edge * e = (Edge*)(edge_local_buf + pos);
-						assert(vertex_map.find(e->src) != vertex_map.end());
-						VertexDataType * src_vertex = vertex_map.find(e->src)->second;
-						src_vertex->degree++;
-					}
-
-				}
-
-				//for debugging
-//				for(size_t off = 0; off < vertex_file_size; off += vertex_unit){
-//					VertexDataType* v = reinterpret_cast<VertexDataType*>(vertex_local_buf + off);
-//					std::cout << *v << std::endl;
+//		template <typename VertexDataType>
+//		void Engine::compute_degree_producer(concurrent_queue<int> * task_queue) {
+//			int partition_id = -1;
+//			while(task_queue->test_pop_atomic(partition_id)) {
+//				int fd_vertex = open((filename + "." + std::to_string(partition_id) + ".vertex").c_str(), O_RDWR);
+//				int fd_edge = open((filename + "." + std::to_string(partition_id)).c_str(), O_RDONLY);
+//				assert(fd_vertex > 0 && fd_edge > 0 );
+//
+//				// get file size
+//				long vertex_file_size = io_manager::get_filesize(fd_vertex);
+//				long edge_file_size = io_manager::get_filesize(fd_edge);
+//
+//				// vertex data fully loaded into memory
+//				char * vertex_local_buf = new char[vertex_file_size];
+//				io_manager::read_from_file(fd_vertex, vertex_local_buf, vertex_file_size, 0);
+//				std::unordered_map<VertexId, VertexDataType*> vertex_map;
+//				load_vertices_hashMap(vertex_local_buf, vertex_file_size, vertex_map);
+//
+//				// streaming edges
+////				char * edge_local_buf = (char *)memalign(PAGE_SIZE, IO_SIZE);
+////				int streaming_counter = edge_file_size / IO_SIZE + 1;
+//				char * edge_local_buf = (char *)memalign(PAGE_SIZE, IO_SIZE * sizeof(Edge));
+//				int streaming_counter = edge_file_size / (IO_SIZE * sizeof(Edge)) + 1;
+//
+//				long valid_io_size = 0;
+//				long offset = 0;
+//
+//				assert(edge_unit == sizeof(Edge));
+//				// for all streaming
+//				for(int counter = 0; counter < streaming_counter; counter++) {
+//
+//					// last streaming
+//					if(counter == streaming_counter - 1)
+//						// TODO: potential overflow?
+////						valid_io_size = edge_file_size - IO_SIZE * (streaming_counter - 1);
+//						valid_io_size = edge_file_size - IO_SIZE * sizeof(Edge) * (streaming_counter - 1);
+//					else
+////						valid_io_size = IO_SIZE;
+//						valid_io_size = IO_SIZE * sizeof(Edge);
+//
+//					assert(valid_io_size % sizeof(edge_unit) == 0);
+//
+//					io_manager::read_from_file(fd_edge, edge_local_buf, valid_io_size, offset);
+//					offset += valid_io_size;
+//
+//					for(long pos = 0; pos < valid_io_size; pos += edge_unit) {
+//						// get an edge
+//						Edge * e = (Edge*)(edge_local_buf + pos);
+//						assert(vertex_map.find(e->src) != vertex_map.end());
+//						VertexDataType * src_vertex = vertex_map.find(e->src)->second;
+//						src_vertex->degree++;
+//					}
+//
 //				}
-
-				// write updated vertex value to disk
-				io_manager::write_to_file(fd_vertex, vertex_local_buf, vertex_file_size);
-
-				// delete
-				delete[] vertex_local_buf;
-				delete[] edge_local_buf;
-				close(fd_vertex);
-				close(fd_edge);
-			}
-		}
+//
+//				//for debugging
+////				for(size_t off = 0; off < vertex_file_size; off += vertex_unit){
+////					VertexDataType* v = reinterpret_cast<VertexDataType*>(vertex_local_buf + off);
+////					std::cout << *v << std::endl;
+////				}
+//
+//				// write updated vertex value to disk
+//				io_manager::write_to_file(fd_vertex, vertex_local_buf, vertex_file_size);
+//
+//				// delete
+//				delete[] vertex_local_buf;
+//				delete[] edge_local_buf;
+//				close(fd_vertex);
+//				close(fd_edge);
+//			}
+//		}
 
 
 		void Engine::read_meta_file(const std::string & filename) {
